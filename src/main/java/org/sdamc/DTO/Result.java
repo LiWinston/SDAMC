cd618cac1d7bf6e2a3e2302c6769eec16e75f723
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-09-13 01:10:59 +1000

    同步14的消息制式 result里套code msg DTO
    完成登录/注册的错误显示

---
+++ src/main/java/org/sdamc/DTO/Result.java
@@ -0,0 +1,71 @@
+package org.sdamc.DTO;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * 后端统一返回结果
+ *
+ * @param <T>
+ */
+@Data
+@Getter
+@Setter
+public class Result<T> implements Serializable {
+
+    private Integer code; // 编码：1成功，0和其它数字为失败
+
+    private String msg; // 错误信息
+
+    private T data; // 数据
+
+    public static <T> Result<T> success() {
+        Result<T> result = new Result<T>();
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> success(T object) {
+        Result<T> result = new Result<T>();
+        result.data = object;
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> success(T object, String msg) {
+        Result<T> result = new Result<T>();
+        result.data = object;
+        result.code = 1;
+        result.msg = msg;
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg) {
+        Result result = new Result();
+        result.msg = msg;
+        result.code = 0;
+        return result;
+    }
+
+    public static <T> Result<T> error(T object, String msg) {
+        Result<T> result = new Result<T>();
+        result.data = object;
+        result.msg = msg;
+        result.code = 0;
+        return result;
+    }
+
+    // 新增方法：用于处理参数验证错误
+    public static <T> Result<T> error(String msg, Map<String, String> errors) {
+        Result<T> result = new Result<>();
+        result.msg = msg;
+        result.code = 0;
+        result.data = (T) errors; // 将错误详情作为数据返回
+        return result;
+    }
+
+}
