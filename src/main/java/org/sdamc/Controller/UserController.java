f08840e3d02b52bb43c23a082f4a93e09805003a
Yongchun Li <yongchunl@student.unimelb.edu.au>
2024-09-13 14:28:20 +1000

    登录后拿管理club列表 有bug
    登录后可以设置本地存储 token和id
    实现了：取id对应可管理的clubs列表
    有问题：/user/' + number + '/clubs' 返回值空

--- src/main/java/org/sdamc/Controller/UserController.java
+++ src/main/java/org/sdamc/Controller/UserController.java
@@ -13,6 +13,7 @@ import org.sdamc.DTO.LoginResponse;
 import org.sdamc.DTO.Result;
 import org.sdamc.DTO.UserDTO;
 import org.sdamc.DomainObject.Students;
+import org.sdamc.Mapper.ClubMembershipsMapper;
 import org.sdamc.Mapper.StudentsMapper;
 import org.sdamc.UnitofWork;
 
@@ -22,11 +23,41 @@ import java.io.IOException;
 @WebServlet(name = "UserController", urlPatterns = { "/user/*" })
 public class UserController extends HttpServlet {
 
+    ClubMembershipsMapper clubMembershipsMapper;
+
     private StudentsMapper studentsMapper;
 
     @Override
     public void init() throws ServletException {
         studentsMapper = new StudentsMapper();
+        clubMembershipsMapper = new ClubMembershipsMapper();
+    }
+
+    @Override
+    // 获取用户管辖的club /user/{stuid}/clubs
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String requestURI = req.getRequestURI();
+        if (requestURI.endsWith("/clubs")) {
+            handleGetClubsAdminedByUser(req, resp);
+        }
+        else {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private void handleGetClubsAdminedByUser(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String pathInfo = req.getPathInfo(); // "/123/clubs"
+        String[] parts = pathInfo.split("/");
+
+        if (parts.length >= 2) {
+            int stuid = Integer.parseInt(parts[1]); // parts[1] is "123"
+            var clubs = clubMembershipsMapper.findClubsAdminedByStudent(stuid);
+            resp.setContentType("application/json");
+            new ObjectMapper().writeValue(resp.getOutputStream(), clubs);
+        } else {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid path");
+        }
     }
 
     @Override
